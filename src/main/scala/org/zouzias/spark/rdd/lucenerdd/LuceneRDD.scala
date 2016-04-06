b5710c479ebd3a5e65517990b0027814f143a941
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-04-06 18:18:23 +0200

    cleanup LuceneRDD as much as possible

--- src/main/scala/org/zouzias/spark/rdd/lucenerdd/LuceneRDD.scala
+++ src/main/scala/org/zouzias/spark/rdd/lucenerdd/LuceneRDD.scala
@@ -25,21 +25,13 @@ import org.zouzias.spark.rdd.lucenerdd.impl.ElasticLuceneRDDPartition
 import scala.reflect.ClassTag
 
 /**
- * A lucene based RDD of key-value `(K, V)` pairs that pre-indexes the entries for fast lookups, joins, and
- * optionally updates. To construct an `IndexedRDD`, use one of the constructors in the
- * [[org.zouzias.spark.rdd.lucenerdd.LuceneRDD object]].
  *
- * @tparam K the key associated with each entry in the set.
- * @tparam V the value associated with each entry in the set.
+ * @tparam T
  */
-class LuceneRDD[K: ClassTag, V: ClassTag](
+class LuceneRDD[T: ClassTag](
     /** The underlying representation of the LuceneRDD as an RDD of partitions. */
-    private val partitionsRDD: RDD[LuceneRDDPartition[K, V]])
-  extends RDD[(K, V)](partitionsRDD.context, List(new OneToOneDependency(partitionsRDD))) {
-
-  require(partitionsRDD.partitioner.isDefined)
-
-  override val partitioner = partitionsRDD.partitioner
+    private val partitionsRDD: RDD[LuceneRDDPartition[T]])
+  extends RDD[(T)](partitionsRDD.context, List(new OneToOneDependency(partitionsRDD))) {
 
   override protected def getPartitions: Array[Partition] = partitionsRDD.partitions
 
@@ -66,30 +58,14 @@ class LuceneRDD[K: ClassTag, V: ClassTag](
   }
 
   /** Provides the `RDD[(K, V)]` equivalent output. */
-  override def compute(part: Partition, context: TaskContext): Iterator[(K, V)] = {
-    firstParent[LuceneRDDPartition[K, V]].iterator(part, context).next.iterator
+  override def compute(part: Partition, context: TaskContext): Iterator[T] = {
+    firstParent[LuceneRDDPartition[T]].iterator(part, context).next.iterator
   }
 
   /** Applies a function to each partition of this LuceneRDD. */
-  private def mapIndexedRDDPartitions[K2: ClassTag, V2: ClassTag](
-      f: LuceneRDDPartition[K, V] => LuceneRDDPartition[K2, V2]): LuceneRDD[K2, V2] = {
+  private def mapIndexedRDDPartitions[T2: ClassTag](
+      f: LuceneRDDPartition[T] => LuceneRDDPartition[T2]): LuceneRDD[T2] = {
     val newPartitionsRDD = partitionsRDD.mapPartitions(_.map(f), preservesPartitioning = true)
-    new LuceneRDD(newPartitionsRDD)
-  }
-
-  /** Applies a function to corresponding partitions of `this` and another LuceneRDD. */
-  private def zipIndexedRDDPartitions[V2: ClassTag, V3: ClassTag](other: LuceneRDD[K, V2])
-      (f: ZipPartitionsFunction[V2, V3]): LuceneRDD[K, V3] = {
-    assert(partitioner == other.partitioner)
-    val newPartitionsRDD = partitionsRDD.zipPartitions(other.partitionsRDD, true)(f)
-    new LuceneRDD(newPartitionsRDD)
-  }
-
-  /** Applies a function to corresponding partitions of `this` and a pair RDD. */
-  private def zipPartitionsWithOther[V2: ClassTag, V3: ClassTag](other: RDD[(K, V2)])
-      (f: OtherZipPartitionsFunction[V2, V3]): LuceneRDD[K, V3] = {
-    val partitioned = other.partitionBy(partitioner.get)
-    val newPartitionsRDD = partitionsRDD.zipPartitions(partitioned, true)(f)
     new LuceneRDD(newPartitionsRDD)
   }
 
@@ -100,199 +76,36 @@ class LuceneRDD[K: ClassTag, V: ClassTag](
    * @param pred the user defined predicate, which takes a tuple to conform to the `RDD[(K, V)]`
    * interface
    */
-  override def filter(pred: Tuple2[K, V] => Boolean): LuceneRDD[K, V] =
-    this.mapIndexedRDDPartitions(_.filter(Function.untupled(pred)))
-
-  /** Maps each value, preserving the index. */
-  def mapValues[V2: ClassTag](f: V => V2): LuceneRDD[K, V2] =
-    this.mapIndexedRDDPartitions(_.mapValues((vid, attr) => f(attr)))
-
-  /** Maps each value, supplying the corresponding key and preserving the index. */
-  def mapValues[V2: ClassTag](f: (K, V) => V2): LuceneRDD[K, V2] =
-    this.mapIndexedRDDPartitions(_.mapValues(f))
+  override def filter(pred: T => Boolean): LuceneRDD[T] = {
+    val newPartitionRDD = partitionsRDD.mapPartitions(partition =>
+      partition.map(_.filter(pred)), preservesPartitioning = true
+    )
+    new LuceneRDD(newPartitionRDD)
+  }
 
   /**
    * Intersects `this` and `other` and keeps only elements with differing values. For these
    * elements, keeps the values from `this`.
    */
-  def diff(other: RDD[(K, V)]): LuceneRDD[K, V] = other match {
-    case other: LuceneRDD[K, V] if partitioner == other.partitioner =>
-      this.zipIndexedRDDPartitions(other)(new DiffZipper)
-    case _ =>
-      this.zipPartitionsWithOther(other)(new OtherDiffZipper)
-  }
+  def diff(other: RDD[T]): LuceneRDD[T] = ???
 
-  /**
-   * Joins `this` with `other`, running `f` on the values of all keys in both sets. Note that for
-   * efficiency `other` must be an IndexedRDD, not just a pair RDD. Use [[???]] to
-   * construct an LuceneRDD co-partitioned with `this`.
-   */
-  def fullOuterJoin[V2: ClassTag, W: ClassTag]
-      (other: RDD[(K, V2)])
-      (f: (K, Option[V], Option[V2]) => W): LuceneRDD[K, W] = other match {
-    case other: LuceneRDD[K, V2] if partitioner == other.partitioner =>
-      this.zipIndexedRDDPartitions(other)(new FullOuterJoinZipper(f))
-    case _ =>
-      this.zipPartitionsWithOther(other)(new OtherFullOuterJoinZipper(f))
-  }
-
-  /**
-   * Left outer joins `this` with `other`, running `f` on the values of corresponding keys. Because
-   * values in `this` with no corresponding entries in `other` are preserved, `f` cannot change the
-   * value type.
-   */
-  def join[U: ClassTag]
-      (other: RDD[(K, U)])(f: (K, V, U) => V): LuceneRDD[K, V] = other match {
-    case other: LuceneRDD[K, U] if partitioner == other.partitioner =>
-      this.zipIndexedRDDPartitions(other)(new JoinZipper(f))
-    case _ =>
-      this.zipPartitionsWithOther(other)(new OtherJoinZipper(f))
-  }
-
-  /** Left outer joins `this` with `other`, running `f` on all values of `this`. */
-  def leftJoin[V2: ClassTag, V3: ClassTag]
-      (other: RDD[(K, V2)])(f: (K, V, Option[V2]) => V3): LuceneRDD[K, V3] = other match {
-    case other: LuceneRDD[K, V2] if partitioner == other.partitioner =>
-      this.zipIndexedRDDPartitions(other)(new LeftJoinZipper(f))
-    case _ =>
-      this.zipPartitionsWithOther(other)(new OtherLeftJoinZipper(f))
-  }
-
-  /** Inner joins `this` with `other`, running `f` on the values of corresponding keys. */
-  def innerJoin[V2: ClassTag, V3: ClassTag](other: RDD[(K, V2)])
-      (f: (K, V, V2) => V3): LuceneRDD[K, V3] = other match {
-    case other: LuceneRDD[K, V2] if partitioner == other.partitioner =>
-      this.zipIndexedRDDPartitions(other)(new InnerJoinZipper(f))
-    case _ =>
-      this.zipPartitionsWithOther(other)(new OtherInnerJoinZipper(f))
-  }
-
-  // The following functions could have been anonymous, but we name them to work around a Scala
-  // compiler bug related to specialization.
-
-  private type ZipPartitionsFunction[V2, V3] = Function2[Iterator[LuceneRDDPartition[K, V]], Iterator[LuceneRDDPartition[K, V2]],
-      Iterator[LuceneRDDPartition[K, V3]]]
-
-  private type OtherZipPartitionsFunction[V2, V3] =
-    Function2[Iterator[LuceneRDDPartition[K, V]], Iterator[(K, V2)],
-      Iterator[LuceneRDDPartition[K, V3]]]
-
-
-  private class DiffZipper extends ZipPartitionsFunction[V, V] with Serializable {
-    def apply(thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[LuceneRDDPartition[K, V]]): Iterator[LuceneRDDPartition[K, V]] = {
-      val thisPart = thisIter.next()
-      val otherPart = otherIter.next()
-      Iterator(thisPart.diff(otherPart))
-    }
-  }
-
-  private class OtherDiffZipper extends OtherZipPartitionsFunction[V, V] with Serializable {
-    def apply(thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[(K, V)]): Iterator[LuceneRDDPartition[K, V]] = {
-      val thisPart = thisIter.next()
-      Iterator(thisPart.diff(otherIter))
-    }
-  }
-
-  private class FullOuterJoinZipper[V2: ClassTag, W: ClassTag](f: (K, Option[V], Option[V2]) => W)
-      extends ZipPartitionsFunction[V2, W] with Serializable {
-    def apply(
-               thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[LuceneRDDPartition[K, V2]])
-        : Iterator[LuceneRDDPartition[K, W]] = {
-      val thisPart = thisIter.next()
-      val otherPart = otherIter.next()
-      Iterator(thisPart.fullOuterJoin(otherPart)(f))
-    }
-  }
-
-  private class OtherFullOuterJoinZipper[V2: ClassTag, W: ClassTag](f: (K, Option[V], Option[V2]) => W)
-      extends OtherZipPartitionsFunction[V2, W] with Serializable {
-    def apply(
-               thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[(K, V2)])
-        : Iterator[LuceneRDDPartition[K, W]] = {
-      val thisPart = thisIter.next()
-      Iterator(thisPart.fullOuterJoin(otherIter)(f))
-    }
-  }
-
-  private class JoinZipper[U: ClassTag](f: (K, V, U) => V)
-      extends ZipPartitionsFunction[U, V] with Serializable {
-    def apply(thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[LuceneRDDPartition[K, U]]): Iterator[LuceneRDDPartition[K, V]] = {
-      val thisPart = thisIter.next()
-      val otherPart = otherIter.next()
-      Iterator(thisPart.join(otherPart)(f))
-    }
-  }
-
-  private class OtherJoinZipper[U: ClassTag](f: (K, V, U) => V)
-      extends OtherZipPartitionsFunction[U, V] with Serializable {
-    def apply(thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[(K, U)]): Iterator[LuceneRDDPartition[K, V]] = {
-      val thisPart = thisIter.next()
-      Iterator(thisPart.join(otherIter)(f))
-    }
-  }
-
-  private class LeftJoinZipper[V2: ClassTag, V3: ClassTag](f: (K, V, Option[V2]) => V3)
-      extends ZipPartitionsFunction[V2, V3] with Serializable {
-    def apply(thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[LuceneRDDPartition[K, V2]]): Iterator[LuceneRDDPartition[K, V3]] = {
-      val thisPart = thisIter.next()
-      val otherPart = otherIter.next()
-      Iterator(thisPart.leftJoin(otherPart)(f))
-    }
-  }
-
-  private class OtherLeftJoinZipper[V2: ClassTag, V3: ClassTag](f: (K, V, Option[V2]) => V3)
-      extends OtherZipPartitionsFunction[V2, V3] with Serializable {
-    def apply(thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[(K, V2)]): Iterator[LuceneRDDPartition[K, V3]] = {
-      val thisPart = thisIter.next()
-      Iterator(thisPart.leftJoin(otherIter)(f))
-    }
-  }
-
-  private class InnerJoinZipper[V2: ClassTag, V3: ClassTag](f: (K, V, V2) => V3)
-      extends ZipPartitionsFunction[V2, V3] with Serializable {
-    def apply(
-               thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[LuceneRDDPartition[K, V2]])
-      : Iterator[LuceneRDDPartition[K, V3]] = {
-      val thisPart = thisIter.next()
-      val otherPart = otherIter.next()
-      Iterator(thisPart.innerJoin(otherPart)(f))
-    }
-  }
-
-  private class OtherInnerJoinZipper[V2: ClassTag, V3: ClassTag](f: (K, V, V2) => V3)
-      extends OtherZipPartitionsFunction[V2, V3] with Serializable {
-    def apply(thisIter: Iterator[LuceneRDDPartition[K, V]], otherIter: Iterator[(K, V2)])
-      : Iterator[LuceneRDDPartition[K, V3]] = {
-      val thisPart = thisIter.next()
-      Iterator(thisPart.innerJoin(otherIter)(f))
-    }
-  }
 }
 
 object LuceneRDD {
   /**
    * Constructs a LuceneRDD from an RDD of pairs, merging duplicate keys arbitrarily.
    */
-  def apply[K: ClassTag, V: ClassTag]
-      (elems: RDD[(K, V)]): LuceneRDD[K, V] = updatable(elems)
+  def apply[T: ClassTag]
+      (elems: RDD[T]): LuceneRDD[T] = {
 
-  /**
-   * Constructs a LuceneRDD from an RDD of pairs, merging duplicate keys arbitrarily.
-   */
-  def updatable[K: ClassTag, V: ClassTag]
-      (elems: RDD[(K, V)])
-    : LuceneRDD[K, V] = updatable[K, V, V](elems, (id, a) => a, (id, a, b) => b)
-
-  /** Constructs an Lucene from an RDD of pairs. */
-  def updatable[K: ClassTag, U: ClassTag, V: ClassTag]
-      (elems: RDD[(K, U)], z: (K, U) => V, f: (K, V, U) => V)
-    : LuceneRDD[K, V] = {
-    val elemsPartitioned =
-      if (elems.partitioner.isDefined) elems
-      else elems.partitionBy(new HashPartitioner(elems.partitions.size))
-    val partitions = elemsPartitioned.mapPartitions[LuceneRDDPartition[K, V]](
-      iter => Iterator(ElasticLuceneRDDPartition(iter, z, f)),
+    val partitions = elems.mapPartitions[LuceneRDDPartition[T]](
+      iter => Iterator(ElasticLuceneRDDPartition(iter)),
       preservesPartitioning = true)
     new LuceneRDD(partitions)
+  }
+
+  def apply[T: ClassTag]
+  (elems: Iterable[T])(implicit sc: SparkContext): LuceneRDD[T] = {
+    apply(sc.parallelize[T](elems.toSeq))
   }
 }
--- src/main/scala/org/zouzias/spark/rdd/lucenerdd/LuceneRDDPartition.scala
+++ src/main/scala/org/zouzias/spark/rdd/lucenerdd/LuceneRDDPartition.scala
@@ -24,94 +24,32 @@ import scala.reflect.ClassTag
  * fast lookups, joins, and optionally updates. To construct an `IndexedRDDPartition`, use one of
  * the constructors in the [[org.zouzias.spark.rdd.lucenerdd.LuceneRDDPartition object]].
  *
- * @tparam K the key associated with each entry in the set.
- * @tparam V the value associated with each entry in the set.
+ * @tparam T the key associated with each entry in the set.
  */
-private[lucenerdd] abstract class LuceneRDDPartition[K, V] extends Serializable {
+private[lucenerdd] abstract class LuceneRDDPartition[T] extends Serializable {
 
-  protected implicit def kTag: ClassTag[K]
-  protected implicit def vTag: ClassTag[V]
+  protected implicit def kTag: ClassTag[T]
 
   def size: Long
 
-  /** Return the value for the given key. */
-  def apply(k: K): V
+  def iterator: Iterator[T]
 
-  def isDefined(k: K): Boolean
-
-  def iterator: Iterator[(K, V)]
-
-  /**
-   * Gets the values corresponding to the specified keys, if any.
-   */
-  def multiget(ks: Iterator[K]): Iterator[(K, V)]
-
-  /** Maps each value, supplying the corresponding key and preserving the index. */
-  def mapValues[V2: ClassTag](f: (K, V) => V2): LuceneRDDPartition[K, V2]
+  def isDefined(key: T): Boolean
 
   /**
    * Restricts the entries to those satisfying the given predicate.
    */
-  def filter(pred: (K, V) => Boolean): LuceneRDDPartition[K, V]
+  def filter(pred: T => Boolean): LuceneRDDPartition[T]
 
   /**
    * Intersects `this` and `other` and keeps only elements with differing values. For these
    * elements, keeps the values from `this`.
    */
-  def diff(other: LuceneRDDPartition[K, V]): LuceneRDDPartition[K, V]
+  def diff(other: LuceneRDDPartition[T]): LuceneRDDPartition[T]
 
   /**
    * Intersects `this` and `other` and keeps only elements with differing values. For these
    * elements, keeps the values from `this`.
    */
-  def diff(other: Iterator[(K, V)]): LuceneRDDPartition[K, V]
-
-  /** Joins `this` with `other`, running `f` on the values of all keys in both sets. */
-  def fullOuterJoin[V2: ClassTag, W: ClassTag]
-      (other: LuceneRDDPartition[K, V2])
-      (f: (K, Option[V], Option[V2]) => W): LuceneRDDPartition[K, W]
-
-  /** Joins `this` with `other`, running `f` on the values of all keys in both sets. */
-  def fullOuterJoin[V2: ClassTag, W: ClassTag]
-      (other: Iterator[(K, V2)])
-      (f: (K, Option[V], Option[V2]) => W): LuceneRDDPartition[K, W]
-
-  /**
-   * Left outer joins `this` with `other`, running `f` on the values of corresponding keys. Because
-   * values in `this` with no corresponding entries in `other` are preserved, `f` cannot change the
-   * value type.
-   */
-  def join[U: ClassTag]
-      (other: LuceneRDDPartition[K, U])
-      (f: (K, V, U) => V): LuceneRDDPartition[K, V]
-
-  /**
-   * Left outer joins `this` with `other`, running `f` on the values of corresponding keys. Because
-   * values in `this` with no corresponding entries in `other` are preserved, `f` cannot change the
-   * value type.
-   */
-  def join[U: ClassTag]
-      (other: Iterator[(K, U)])
-      (f: (K, V, U) => V): LuceneRDDPartition[K, V]
-
-  /** Left outer joins `this` with `other`, running `f` on all values of `this`. */
-  def leftJoin[V2: ClassTag, V3: ClassTag]
-      (other: LuceneRDDPartition[K, V2])
-      (f: (K, V, Option[V2]) => V3): LuceneRDDPartition[K, V3]
-
-  /** Left outer joins `this` with `other`, running `f` on all values of `this`. */
-  def leftJoin[V2: ClassTag, V3: ClassTag]
-      (other: Iterator[(K, V2)])
-      (f: (K, V, Option[V2]) => V3): LuceneRDDPartition[K, V3]
-
-  /** Inner joins `this` with `other`, running `f` on the values of corresponding keys. */
-  def innerJoin[U: ClassTag, V2: ClassTag]
-      (other: LuceneRDDPartition[K, U])
-      (f: (K, V, U) => V2): LuceneRDDPartition[K, V2]
-
-  /** Inner joins `this` with `other`, running `f` on the values of corresponding keys. */
-  def innerJoin[U: ClassTag, V2: ClassTag]
-      (other: Iterator[(K, U)])
-      (f: (K, V, U) => V2): LuceneRDDPartition[K, V2]
-
+  def diff(other: Iterator[T]): LuceneRDDPartition[T]
 }

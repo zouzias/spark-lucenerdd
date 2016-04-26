c740fc41131c420d5d958d41cbbdf6877ef536f3
Anastasios Zouzias <zouzias@gmail.com>
2016-04-26 09:11:20 +0200

    scripts: fix implicits

--- scripts/loadWords.scala
+++ scripts/loadWords.scala
@@ -1,6 +1,6 @@
 import scala.io.Source
 import org.zouzias.spark.rdd.lucenerdd.impl.RamLuceneRDDPartition
-import org.zouzias.spark.rdd.lucenerdd.impl.RamLuceneRDDPartition._
+import org.zouzias.spark.rdd.lucenerdd.implicits.LuceneRDDImplicits._
 import org.zouzias.spark.rdd.lucenerdd.LuceneRDD._
 import org.zouzias.spark.rdd.lucenerdd.LuceneRDD
 val words = Source.fromFile("src/test/resources/words.txt").getLines().toSeq

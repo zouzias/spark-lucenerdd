5b5bd9b40eb8f7f49ebe4daf39eb44ff4fda04f3
Anastasios Zouzias <zouzias@gmail.com>
2016-08-16 08:08:07 +0200

    scripts: add solr spatial data linkage

---
+++ scripts/spatial/loadSolrSpatialData.scala
@@ -0,0 +1,38 @@
+
+
+import java.io.StringReader
+
+import com.spatial4j.core.context.jts.JtsSpatialContext
+import com.spatial4j.core.io.ShapeIO
+import org.apache.spark.rdd.RDD
+import org.zouzias.spark.lucenerdd.spatial.shape._
+import org.zouzias.spark.lucenerdd.spatial.shape.ShapeLuceneRDD
+import org.zouzias.spark.lucenerdd._
+import org.zouzias.spark.lucenerdd.LuceneRDD
+
+import scala.reflect.ClassTag
+
+
+// Load all countries
+val allCountries = sqlContext.read.parquet("data/countries-poly.parquet").select("name", "shape").map(row => (row.getString(1), row.getString(0)))
+
+// Load all cities
+val citiesPoint = sqlContext.read.parquet("data/world-cities-points.parquet").select("city", "shape").map(row => (row.getString(1), row.getString(0)))
+
+def parseDouble(s: String): Double = try { s.toDouble } catch { case _: Throwable => 0.0 }
+
+def coords(city: (String, String)): (Double, Double) = {
+  val str = city._1
+  val nums = str.dropWhile(x => x.compareTo('(') != 0).drop(1).dropRight(1)
+  val coords = nums.split(" ").map(_.trim)
+  (parseDouble(coords(0)), parseDouble(coords(1)))
+}
+
+val shapes = ShapeLuceneRDD(allCountries)
+shapes.cache
+
+
+val linked = shapes.linkByKnn(citiesPoint, coords, 3)
+linked.cache
+
+linked.map(x => (x._1, x._2.head.doc.textField("_1"))).foreach(println)

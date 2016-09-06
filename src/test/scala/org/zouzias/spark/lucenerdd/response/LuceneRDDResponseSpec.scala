fa19374f896c9ce2c9d25b0ac3e44aed8113a038
Anastasios Zouzias <zouzias@gmail.com>
2016-09-06 09:00:55 +0200

    luceneRDDResponse: add unit tests

---
+++ src/test/scala/org/zouzias/spark/lucenerdd/response/LuceneRDDResponseSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zouzias.spark.lucenerdd.response
+
+import com.holdenkarau.spark.testing.SharedSparkContext
+import org.apache.spark.SparkConf
+import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
+import org.zouzias.spark.lucenerdd.{LuceneRDD, LuceneRDDKryoRegistrator}
+
+class LuceneRDDResponseSpec extends FlatSpec with Matchers
+  with BeforeAndAfterEach
+  with SharedSparkContext {
+
+  override val conf = LuceneRDDKryoRegistrator.registerKryoClasses(new SparkConf().
+    setMaster("local[*]").
+    setAppName("test").
+    set("spark.ui.enabled", "false").
+    set("spark.app.id", appID))
+
+  def randomString(length: Int): String = scala.util.Random.alphanumeric.take(length).mkString
+  val array = (1 to 24).map(randomString(_))
+
+  var luceneRDD: LuceneRDD[_] = _
+
+  override def afterEach() {
+    luceneRDD.close()
+  }
+
+  "LuceneRDDResponseSpec.take(k)" should "return exactly k elements" in {
+    val array = Array("aaa", "bbb", "ccc", "ddd", "eee")
+    val rdd = sc.parallelize(array)
+    luceneRDD = LuceneRDD(rdd)
+    luceneRDD.take(2).size should be (2)
+  }
+
+  "LuceneRDDResponseSpec.collect()" should "return all elements" in {
+    val array = Array("aaa", "bbb", "ccc", "ddd", "eee")
+    val rdd = sc.parallelize(array)
+    luceneRDD = LuceneRDD(rdd)
+    luceneRDD.collect().length should be (array.size)
+  }
+
+}

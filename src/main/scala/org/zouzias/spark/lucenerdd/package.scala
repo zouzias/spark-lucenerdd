8015b4dd61de66f2a2415f041b03b14a153f9698
Anastasios Zouzias <zouzias@gmail.com>
2019-04-21 12:20:53 +0200

    hotfix: stringToDocument implicit

--- src/main/scala/org/zouzias/spark/lucenerdd/package.scala
+++ src/main/scala/org/zouzias/spark/lucenerdd/package.scala
@@ -98,6 +98,7 @@ package object lucenerdd extends LuceneRDDConfigurable {
 
     if (s != null) {
       doc.add(new Field(DefaultFieldName, s, analyzedField(StringFieldsDefaultAnalyzed)))
+      doc.add(new Field(DefaultFieldName, s))
     }
     doc
   }

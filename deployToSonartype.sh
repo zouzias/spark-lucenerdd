93f9d802800ae84b0efa1fd360c646e8a77ecfc5
Anastasios Zouzias <zouzias@gmail.com>
2019-07-14 09:49:04 +0200

    [release-info] add sonatype credentials

--- deployToSonartype.sh
+++ deployToSonartype.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+## Setup
 
-# Run sbt release to release signed both 2.10 and 2.11
+Add `sonatype.sbt` file under `~/.sbt/1.0/` folder with contents
+
+```
+credentials += Credentials("Sonatype Nexus Repository Manager",
+                           "oss.sonatype.org",
+                           "zouzias",
+                           "PASSWORD_HERE")
+```
+
+## Run sbt release to release signed both 2.10 and 2.11
 sbt release
 
 
-# Then, git checkout v0.X.X to the release tag first, and then type
+## Then, git checkout v0.X.X to the release tag first, and then type
 
 sbt sonatypeRelease
 
-# This will allow sonatype to release the artifacts to maven central.
-# An alternative is to browse to https://oss.sonatype.org and do it manually
+## This will allow sonatype to release the artifacts to maven central.
+## An alternative is to browse to https://oss.sonatype.org and do it manually

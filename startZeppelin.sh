11abc823310947284736f25b1675e4ab8a57ec55
Anastasios Zouzias <zouzias@gmail.com>
2022-03-28 08:59:31 +0200

    [scripts] remove docker-machine cmd

--- startZeppelin.sh
+++ startZeppelin.sh
@@ -3,19 +3,10 @@
 #############################################################
 # Start Zeppelin using Docker                               #
 #############################################################
-# Note: To run this, you need:                              #
-# (1) Docker installed, i.e., docker-machine is available   #
-# (2) There is a "default" docker machine available         #
-#############################################################
-
-DOCKER_NAME="default"
-docker-machine start ${DOCKER_NAME}
-DOCKER_MACHINE_IP=`docker-machine ip ${DOCKER_NAME}`
-eval $(docker-machine env ${DOCKER_NAME})
 
 echo "==========================================="
 echo "==========================================="
-echo "Browse to http://${DOCKER_MACHINE_IP}:8080/"
+echo "Browse to http://localhost:8080/"
 echo "==========================================="
 echo "==========================================="
 

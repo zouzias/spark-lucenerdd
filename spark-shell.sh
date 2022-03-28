8886aca47e5914c3d7735548029dbcdfb425a86d
Anastasios Zouzias <zouzias@gmail.com>
2022-03-28 08:56:06 +0200

    [spark-shell] bump to spark 3.2.1

--- spark-shell.sh
+++ spark-shell.sh
@@ -6,7 +6,7 @@ CURRENT_DIR=`pwd`
 SPARK_LUCENERDD_VERSION=`cat version.sbt | awk '{print $5}' | xargs`
 
 # You should have downloaded this spark version under your ${HOME}
-SPARK_VERSION="3.2.0"
+SPARK_VERSION="3.2.1"
 
 echo "==============================================="
 echo "Loading LuceneRDD with version ${SPARK_LUCENERDD_VERSION}"
